de56a6ecc1a6ffed7bc767f64bb1c0660107683f
harry.python <harry.python@f588eb96-26aa-f4a9-e71f-ee642e46471c>
2010-08-29 16:19:19 +0000

    rename proxy to proxy_postgresql.py, remove many useless code, and change chinese to english.

--- trunk/pgsql_rpc/pgpro.py
+++ trunk/pgsql_rpc/pgpro.py
@@ -34,7 +34,7 @@ INT32=lambda i:struct.pack('!l',i)
 salt=lambda length:''.join([chr(random.choice(range(33,120))) for x in range(length)])
 
 def extract_packet(_buffer):
-    """按照PostgreSQL的协议进行解包"""
+    """extract PostgreSQL protocol packet"""
     if len(_buffer)>=5:
         mtype=_buffer[0]
         msglen=struct.unpack('!L',_buffer[1:5])[0]
@@ -43,7 +43,7 @@ def extract_packet(_buffer):
     return None,None,_buffer
 
 class PGBuffer(object):
-    """按照PostgreSQL协议定义的缓存管理"""
+    """Manage buffer and get PostgreSQL protocol packet"""
     _buffer=''
 
     def __init__(self):
@@ -140,131 +140,19 @@ class PGProtocol(protocol.Protocol):
                     assert packet==''
                     self.transport.loseConnection()
                 else:
-                    print 'UnknownQuery[%d]: mtype=%s, packet=%s'%repr(len(packet),repr(mtype),repr(packet))
+                    print 'UnknownQuery[%d]: mtype=%s, packet=%s'%repr(
+                            len(packet),repr(mtype),repr(packet))
                     self.transport.loseConnection()
             else:
-                print 'UnknownPacket: mtype=%s,packet=%s'%(repr(mtype),repr(packet))
+                print 'UnknownPacket: mtype=%s,packet=%s'%(
+                        repr(mtype),repr(packet))
                 self.transport.loseConnection()
-        return
-
-    def _dataReceived(self,data):
-        self._buffer+=data
-        print 'DataReceived()=%s'%repr(data)
-        if len(self._buffer)>=5:
-            if self._status==ProSts_AskSSL:
-                #询问SSL的部分直接拒绝掉
-                pktlen=struct.unpack('!L',self._buffer[:4])[0]
-                if len(self._buffer)>=pktlen:
-                    pktbuf=self._buffer[4:pktlen]
-                    self._buffer=self._buffer[pktlen:]
-                    if pktbuf[:4]=='\x00\x03\x00\x00':
-                        #self._status=ProSts_WaitAuthClearText
-                        #self.sendPacket('R',INT32(3))        #要求clear-text密码
-                        self._status=ProSts_WaitAuthMD5
-                        self.sendPacket('R',INT32(5)+self.saltstr)
-                    elif pktbuf[:4]=='\x04\xd2\x16\x2f':
-                        self.transport.write('N')
-                        self._status=ProSts_WaitStartup
-            elif self._status==ProSts_WaitStartup:
-                pktlen=struct.unpack('!L',self._buffer[:4])[0]
-                if len(self._buffer)>=pktlen:
-                    pktbuf=self._buffer[4:pktlen]
-                    self._buffer=self._buffer[pktlen:]
-                    protocol_version=pktbuf[:4]
-                    pairlist=pktbuf[4:].split('\x00')[:-2]
-                    #print 'StartUp[%d]: %s'%(len(pktbuf),repr(pktbuf))  #8.3发来的是\x04\xd2\x16\x2f
-                    #print 'Startup[%d]: ProtoVer=%s Pairlist=%s'%(len(pktbuf),repr(protocol_version),repr(pairlist))
-                    #print 'Pairlist: %s'%repr(pairlist)
-                    infodict={}
-                    for idx in range(len(pairlist)/2):
-                        infodict[pairlist[idx*2]]=pairlist[idx*2+1]
-                    self.username=infodict['user']
-                    self.cmdmapping['startup'](protocol_version,infodict)
-                    #self.sendPacket('R',struct.pack('!l',5)+'aaaa') #要求MD5认证
-                    #self.sendPacket('R',INT32(3))        #要求clear-text密码
-                    self._status=ProSts_WaitAuthMD5
-                    self.sendPacket('R',INT32(5)+self.saltstr)
-            elif self._status==ProSts_WaitAuthClearText:
-                pkttype=self._buffer[0]
-                pktlen=struct.unpack('!L',self._buffer[1:5])[0]
-                #print 'WaitAuth[%s][%d]'%(repr(pkttype),pktlen)
-                if len(self._buffer)>=pktlen+1:
-                    pktbuf=self._buffer[5:pktlen+1]
-                    self._buffer=self._buffer[pktlen+1:]
-                    password=pktbuf[:-1]
-                    print 'Auth[%d]: %s, password=%s'%(len(pktbuf),repr(pktbuf),password)
-                    assert pkttype=='p','PasswordMessage must have pkttype [%s]!=[p]'%repr(pkttype)
-                    assert pktbuf[-1]=='\x00'
-                    #self.sendPacket('R',INT32(0))
-                    #self.sendPacket('Z',INT32(5)+'I')
-                    self._status=ProSts_WaitQuery
-                    self.sendPacket('R',INT32(0))
-                    self.sendPacket('S','client_encoding\x00UTF8\x00')
-                    self.sendPacket('S','DateStyle\x00ISO, YMD\x00')
-                    self.sendPacket('S','integer_datetimes\x00on\x00')
-                    self.sendPacket('S','is_superuser\x00on\x00')
-                    self.sendPacket('S','server_encoding\x00UTF8\x00')
-                    self.sendPacket('S','server_version\x008.3.11\x00')
-                    self.sendPacket('S','session_authorization\x00postgres\x00')
-                    self.sendPacket('S','standard_conforming_strings\x00off\x00')
-                    self.sendPacket('S','TimeZone\x00PRC\x00')
-                    self.sendPacket('K','\x00\x00&\xefY3>\xc1')
-                    self.sendPacket('Z','I')
-            elif self._status==ProSts_WaitAuthMD5:
-                pkttype=self._buffer[0]
-                pktlen=struct.unpack('!L',self._buffer[1:5])[0]
-                if len(self._buffer)>=pktlen+1:
-                    pktbuf=self._buffer[5:pktlen+1]
-                    self._buffer=self._buffer[pktlen+1:]
-                    print 'AuthMD5[%d]: %s'%(len(pktbuf),repr(pktbuf))
-                    if self.cmdmapping['authmd5'](self.username,self.saltstr,pktbuf[3:-1]):
-                        self._status=ProSts_WaitQuery
-                        self.sendPacket('R',INT32(0))
-                        for (k,v) in self.cmdmapping['deslist']:
-                            self.sendPacket('S','%s\x00%s\x00'%(k,v))
-                        self.sendPacket('K','\x00\x00&\xefY3>\xc1')
-                        self.sendPacket('Z','I')
-                    else:
-                        #认证失败，干掉连接
-                        #print 'Failed'
-                        #self.sendPacket('E','Fuck you!')
-                        self.sendPacket('E','S\xe8\x87\xb4\xe5\x91\xbd\xe9\x94\x99\xe8\xaf\xaf\x00C28000\x00M\xe7\x94\xa8\xe6\x88\xb7 "dbu" Password \xe8\xae\xa4\xe8\xaf\x81\xe5\xa4\xb1\xe8\xb4\xa5\x00Fauth.c\x00L1017\x00Rauth_failed\x00\x00')
-                        self.transport.loseConnection()
-            elif self._status==ProSts_WaitQuery:
-                pkttype=self._buffer[0]
-                pktlen=struct.unpack('!L',self._buffer[1:5])[0]
-                if len(self._buffer)>=pktlen+1:
-                    pktbuf=self._buffer[5:pktlen+1]
-                    self._buffer=self._buffer[pktlen+1:]
-                    if pkttype=='Q':
-                        #查询
-                        query=pktbuf[:-1]
-                        print 'Query[%d]: %s, query=%s'%(len(pktbuf),repr(pktbuf),query)
-                        self.sendPacket('T','\x00\x01idx\x00\x00\x00@\x00\x00\x01\x00\x00\x00\x17\x00\x04\xff\xff\xff\xff\x00\x00')
-                        self.sendPacket('C','SELECT\x00')
-                        self.sendPacket('Z','I')
-                    elif pkttype=='X':
-                        #关闭
-                        assert pktbuf==''
-                        self.transport.loseConnection()
-                    else:
-                        print 'UCommand[%d]: %s'%(len(pktbuf),repr(pktbuf))
-            else:
-                pkttype=self._buffer[0]
-                pktlen=struct.unpack('!L',self._buffer[1:5])[0]
-                if len(self._buffer)>=pktlen+1:
-                    pktbuf=self._buffer[5:pktlen+1]
-                    self._buffer=self._buffer[pktlen+1:]
-                    print 'Unknown[%s][%d]: %s'%(repr(pkttype),len(pktbuf),repr(pktbuf))
         return
 
     def sendPacket(self,pkttype,pktbuf):
         databuf=pkttype+struct.pack('!L',len(pktbuf)+4)+pktbuf
         #print 'Sent[%d]: %s'%(len(databuf),repr(databuf))
         self.transport.write(databuf)
-        return
-
-    def queryReceived(self,query):
         return
 
 class PGFactory(protocol.ServerFactory):
